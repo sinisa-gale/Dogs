bddcadbbbbc0044de66ab3e02bf4f7791eb2795c
Sinisa Gacic <73501410+sinisa-gale@users.noreply.github.com>
2021-03-07 17:22:50 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,20 @@ var selector = document.querySelector('select');
 var dogBreed = selector.value;
 
 var selectChange = function(e) {
-		dogBreed = e.target.value;
+	clearInterval(timer);
+	dogBreed = e.target.value;
+	var timer = setInterval(requestHttp, 5000);
+}
+
+var changeImage = function (imageSrc) {
+	var imgElement = document.querySelector('img');
+	if (imgElement) {
+		imgElement.setAttribute('src', imageSrc);
+	} else {
+		var imgElem = document.createElement('img');
+		imgElem.setAttribute('src', imageSrc);
+		document.querySelector('.image-container').appendChild(imgElem);
+	}
 }
 
 function requestHttp() {
@@ -16,41 +29,3 @@ function requestHttp() {
 }
 
 selector.addEventListener('change', selectChange);
-
-
-// var addObject = function (parentNode, imageSrc, movTitle) {
-// 	var divElem = document.createElement('div');
-// 	divElem.innerHTML = '<img src="./img/' + imageSrc +
-// 	'" class="movie"><img src="./img/close.png" class="close"><p>' + movTitle + '</p>';
-// 	parentNode.appendChild(divElem);
-// }
-//
-// var deleteFunc = function(e) {
-// 	e.target.removeEventListener('click', deleteFunc);
-// 	var selectedNode = e.target.parentNode;
-// 	var searchTitle = selectedNode.querySelector('p').innerHTML;
-// 	movieList.forEach(function(item, index) { searchTitle === item.title ? movieList.splice(index, 1) : {}; });
-// 	selectedNode.parentNode.removeChild(selectedNode);
-// }
-//
-// var elementLoad = function(parentNode, arr) {
-// 	arr.forEach(function(item) { addObject(parentNode, item.picture, item.title); })
-// 	var closeList = document.querySelectorAll('.close');
-// 	closeList.forEach(function(item) {item.addEventListener('click', deleteFunc);})
-// }
-//
-// var filtering = function(e) {
-// 	var oldChild = document.getElementsByClassName('container');
-// 	var sec = document.createElement('section');
-// 	sec.setAttribute('class','container');
-// 	document.querySelector('main').replaceChild(sec, oldChild[0]);
-// 	var filterValue = e.target.value;
-// 	var tempArr = movieList.filter(function(item) {
-// 		var found = item.title.toUpperCase().search(filterValue.toUpperCase());
-// 		if (found > -1) {return item};
-// 	});
-// 	elementLoad(document.querySelector('.container'), tempArr);
-// }
-//
-// elementLoad(document.querySelector('.container'), movieList);
-// document.querySelector('input').addEventListener('keyup', filtering);
